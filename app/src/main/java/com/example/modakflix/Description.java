913072cf3a35a142bd1785c130b99b36048728ec
Sourav Modak <72427070+official-srv-modak@users.noreply.github.com>
2021-09-18 12:04:33 +0530

    UI changes and bug fixes

--- app/src/main/java/com/example/modakflix/Description.java
+++ app/src/main/java/com/example/modakflix/Description.java
@@ -328,7 +328,7 @@ public class Description extends AppCompatActivity {
                         intent.setPackage("com.mxtech.videoplayer.ad"); // com.mxtech.videoplayer.pro
                         intent.putExtra("position", pos1);
                         byte decoder = 2;
-                        //intent.putExtra("decode_mode", decoder);
+                        intent.putExtra("decode_mode", decoder);
                         //intent.putExtra("fast_mode", true);
                         intent.putExtra("return_result", true);
                         startActivityForResult(intent, 1);
@@ -399,7 +399,7 @@ public class Description extends AppCompatActivity {
                         intent.setPackage("com.mxtech.videoplayer.ad"); // com.mxtech.videoplayer.pro
                         intent.putExtra("position", pos);
                         byte decoder = 2;
-                        //intent.putExtra("decode_mode", decoder);
+                        intent.putExtra("decode_mode", decoder);
                         //intent.putExtra("fast_mode", true);
                         intent.putExtra("return_result", true);
                         startActivityForResult(intent, 1);
